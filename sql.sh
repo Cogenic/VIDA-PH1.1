3accdd1fac09e8a6a6916fe38fdefd4f11c1e0d6
Kirubel D. Tesfaye <35328807+KirubelDTesfaye@users.noreply.github.com>
2020-08-01 13:27:07 -0700

    fixed database connection issues

--- sql.sh
+++ sql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
+
 #sudo mkdir /cloudsql; sudo chmod 777 /cloudsql
 #./cloud_sql_proxy -dir=/cloudsql -instances=cogenicintel:us-west4-a:reason \ -credential_file=cogenic-sql-1.json
  #mysql -u Kirubel -p login_info -S /cloudsql/cogenic:us-west2:cogenic-sql-1
