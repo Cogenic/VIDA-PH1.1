ab62e9f6178701c994b95972af9fa5c88c165efa
Kirubel D. Tesfaye <35328807+KirubelDTesfaye@users.noreply.github.com>
2021-01-10 14:03:42 -0800

    Delete Startup.cs

--- Startup.cs
+++
@@ -1,8 +0,0 @@
-var connectionString = new MySqlConnectionStringBuilder(
-    Configuration["CloudSql:ConnectionString"])
-{
-    // Connecting to a local proxy that does not support ssl.
-    SslMode = MySqlSslMode.None,
-};
-DbConnection connection =
-    new MySqlConnection(connectionString.ConnectionString);
